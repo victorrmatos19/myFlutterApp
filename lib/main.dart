2a55829ec243f1753c55ecf96318fe3f0687b0c2
x229466 <victor.r.matos@accenture.com>
2019-12-14 20:52:36 -0300

    refactoring code :recycle:

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
     home: Scaffold(
+      body: ListaTrasferencia(),
       appBar: AppBar(title: Text("Transferências"),),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -12,4 +13,38 @@ void main() => runApp(
   )
 );
 
+class ListaTrasferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTrasferencia(Transferencia(100.00,1000)),
+        ItemTrasferencia(Transferencia(300.00,2030)),
+      ],
+    );
+  }
+}
 
+class ItemTrasferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTrasferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(_transferencia.value.toString()),
+          subtitle: Text(_transferencia.numberAccount.toString()),
+        )
+    );
+  }
+}
+
+class Transferencia {
+  double value;
+  int numberAccount;
+
+  Transferencia(this.value,this.numberAccount);
+}
