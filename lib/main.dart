5f73e6aa421041aa0b9eaa01e5d54fc4a5e361b6
x229466 <victor.r.matos@accenture.com>
2019-12-15 01:38:40 -0300

    refactoring code :recycle:

--- lib/main.dart
+++ lib/main.dart
@@ -15,46 +15,64 @@ class ByteBankApp  extends StatelessWidget{
 }
 
 class FormularioTransferencia  extends StatelessWidget{
+  final TextEditingController _controllerInputValue = TextEditingController();
+  final TextEditingController _controllerInputNumberAccount = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Criando Transferência"),),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              style: TextStyle(
-                fontSize: 24.0
-              ),
-              decoration: InputDecoration(
-                labelText: "Número da Conta",
-                hintText: "0001"
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              style: TextStyle(
-                fontSize: 24.0
-              ),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: "Valor",
-                hintText: "0.00"
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          Editor(controller: _controllerInputNumberAccount,labelText: "Numero da Conta",hintText: "0001",),
+          Editor(controller: _controllerInputValue,labelText: "Valor",hintText: "0.00",icone: Icons.monetization_on,),
           RaisedButton(
+            onPressed: () => _createTransfer(_controllerInputValue.text,_controllerInputNumberAccount.text),
             child: Text("Confirmar"),
           ),
         ],
       ),
     );
   }
+
+  void _createTransfer(String inputValue, String InputNumberAccount) {
+    final double value = double.tryParse(inputValue);
+    final int numberAccount = int.tryParse(InputNumberAccount);
+
+    if(value != null && numberAccount!= null ){
+      final trasferenciaCriada = Transferencia(value, numberAccount);
+    }
+  }
 }
+
+class Editor extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String hintText;
+  final IconData icone;
+
+  Editor({this.controller, this.labelText, this.hintText,this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(
+            fontSize: 24.0
+        ),
+        decoration: InputDecoration(
+            icon: icone != null ?  Icon(icone) : null,
+            labelText: labelText,
+            hintText: hintText,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
+}
+
 
 class ListaTransferencia extends StatelessWidget {
   @override
@@ -96,4 +114,9 @@ class Transferencia {
   int numberAccount;
 
   Transferencia(this.value,this.numberAccount);
+
+  @override
+  String toString() {
+    return 'Transferencia{value: $value, numberAccount: $numberAccount}';
+  }
 }
